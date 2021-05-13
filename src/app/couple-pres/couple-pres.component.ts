37405c004b2fdc6efd340f15013cac7e7d279310
Malek BENAZZOUZ <benazzouzmalek@gmail.com>
2021-05-13 19:42:52 +0200

    Add all the sections, with basic content

---
+++ src/app/couple-pres/couple-pres.component.ts
@@ -0,0 +1,16 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-couple-pres',
+  templateUrl: './couple-pres.component.html',
+  styleUrls: ['./couple-pres.component.scss']
+})
+export class CouplePresComponent implements OnInit {
+
+  cardsXMargin: number;
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+}
