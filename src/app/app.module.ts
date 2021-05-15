a5338ed48ed096d45add9c747763f99c4e648b11
Malek BENAZZOUZ <benazzouzmalek@gmail.com>
2021-05-15 10:10:39 +0200

    Add nav links to header, with smooth scroll to anchors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { CountdownComponent } from './countdown/countdown.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { LayoutModule } from '@angular/cdk/layout';
 import { IntroComponent } from './intro/intro.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { CouplePresComponent } from './couple-pres/couple-pres.component';
@@ -22,7 +16,7 @@ import { ProgramComponent } from './program/program.component';
 import { AccommodationComponent } from './accommodation/accommodation.component';
 import { RsvpFormComponent } from './rsvp-form/rsvp-form.component';
 import { MoneyPotComponent } from './money-pot/money-pot.component';
-import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarouselModule, NgbCollapseModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatOptionModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
@@ -50,18 +44,14 @@ import { BonusComponent } from './bonus/bonus.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatToolbarModule,
     MatIconModule,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule,
-    LayoutModule,
-    NgbCarouselModule,
     MatFormFieldModule,
-    MatOptionModule,
+    MatInputModule,
     MatSelectModule,
-    MatInputModule
+    MatOptionModule,
+    NgbCarouselModule,
+    NgbNavModule,
+    NgbCollapseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
