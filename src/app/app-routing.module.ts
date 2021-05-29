351636a97ed58be1f7664834b4d50905a694a159
Malek BENAZZOUZ <benazzouzmalek@gmail.com>
2021-05-29 18:59:53 +0200

    Create an admin page, where we display the rsvp confirmations if authenticated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
